c474ef7415ac9b2034b0688efe8eb38cb63194e7
hnguyen04 <danghieunguyen004@gmail.com>
2025-12-23 20:03:19 +0700

    fix submit flink

--- scripts/streaming/kafka_to_big_query_local.py
+++ scripts/streaming/kafka_to_big_query_local.py
@@ -4,7 +4,6 @@ from ingestion.finnhub_client import start_finnhub
 # from ingestion.vnstock_client import start_vnstock_polling
 from config.settings import settings
 from processing.flink_job_local import kafka_to_bigquery_job
-from vnstock import Vnstock
 
 logging.basicConfig(level=settings.LOG_LEVEL)
 logger = logging.getLogger(__name__)
